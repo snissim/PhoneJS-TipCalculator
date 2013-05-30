95a307c36d6b866d4014d14aa876be35d596dba2
Aleksey Martynov <bclexa@gmail.com>
2013-05-30 11:57:06 +0400

    View model refactored

--- views/home.js
+++ views/home.js
@@ -1,83 +1,92 @@
 ﻿"use strict";
 
 TipCalculator.home = function(params) {
-    var DEFAULT_TIP_PERCENT = 15,
-        ROUND_UP = 1,
-        ROUND_DOWN = -1,
-        ROUD_NONE = 0;
+    var DEFAULT_TIP_PERCENT = 15;
 
     var billTotal = ko.observable(),
-        roundMode = ko.observable(ROUD_NONE),
         tipPercent = ko.observable(DEFAULT_TIP_PERCENT),
         splitNum = ko.observable(1);
+
+    var ROUND_UP = 1,
+        ROUND_DOWN = -1,
+        ROUND_NONE = 0,
+        roundMode = ko.observable(ROUND_NONE);
 
     function billTotalAsNumber() {
         return billTotal() || 0;
     }
 
-    billTotal.subscribe(function() {
-        roundMode(ROUD_NONE);
-    });
-
-    tipPercent.subscribe(function() {
-        roundMode(ROUD_NONE);
-    });
-
-    splitNum.subscribe(function() {
-        roundMode(ROUD_NONE);
-    });
 
     var totalTip = ko.computed(function() {
-        return tipPercent() / 100 * billTotalAsNumber();
-    });
-
-    var totalPerPerson = ko.computed(function() {
-        var personSum = (totalTip() + billTotalAsNumber()) / splitNum();
-        switch(roundMode()) {
-            case ROUND_DOWN:
-                if(Math.floor(personSum) * splitNum() >= billTotalAsNumber()) {
-                    return Math.floor(personSum);
-                } else {
-                    return personSum;
-                }
-                break
-            case ROUND_UP:
-                return Math.ceil(personSum);
-            case ROUD_NONE:
-                return personSum;
-        }
-    });
-
-    var totalToPay = ko.computed(function() {
-        return totalPerPerson() * splitNum();
+        return 0.01 * tipPercent() * billTotalAsNumber();
     });
 
     var tipPerPerson = ko.computed(function() {
         return totalTip() / splitNum();
     });
 
+    var totalPerPerson = ko.computed(function() {
+        return (totalTip() + billTotalAsNumber()) / splitNum();
+    });
+
+    var totalToPay = ko.computed(function() {
+        var value = totalTip() + billTotalAsNumber();
+
+        switch(roundMode()) {
+            case ROUND_DOWN:
+                if(Math.floor(value) >= billTotalAsNumber())
+                    return Math.floor(value);
+                return value;
+
+            case ROUND_UP:
+                return Math.ceil(value);
+
+            default:
+                return value;
+        }
+    });
+
+
     function roundUp() {
         roundMode(ROUND_UP);
     }
-        
+
     function roundDown() {
         roundMode(ROUND_DOWN);
     }
+
+
+    billTotal.subscribe(function() {
+        roundMode(ROUND_NONE);
+    });
+
+    tipPercent.subscribe(function() {
+        roundMode(ROUND_NONE);
+    });
+
+    splitNum.subscribe(function() {
+        roundMode(ROUND_NONE);
+    });
+
 
     function viewShown() {
         $("#billTotalInput").data("dxNumberBox").focus();
     }
 
+
     return {
-        viewShown: viewShown,
         billTotal: billTotal,
-        totalTip: totalTip,
-        totalToPay: totalToPay,
-        totalPerPerson: totalPerPerson,
-        tipPerPerson:tipPerPerson,
         tipPercent: tipPercent,
         splitNum: splitNum,
+
+        totalTip: totalTip,
+        tipPerPerson: tipPerPerson,
+        totalPerPerson: totalPerPerson,
+        totalToPay: totalToPay,
+
         roundUp: roundUp,
-        roundDown: roundDown
+        roundDown: roundDown,
+
+        viewShown: viewShown
     };
 };

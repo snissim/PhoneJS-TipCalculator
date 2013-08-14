885ddc173bf957256619e8a84d9dcdb7088f86ad
Aleksey Martynov <bclexa@gmail.com>
2013-08-14 15:40:28 +0400

    Updated to PhoneJS 13.1.6

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ $(function() {
     document.addEventListener("deviceready", function() { navigator.splashscreen.hide(); });
 
     TipCalculator.app = new DevExpress.framework.html.HtmlApplication({
-        namespace: TipCalculator,        
+        namespace: TipCalculator,
+        
         defaultLayout: "empty"
     });
+
+    // enable iOS7 theme
+    var device = DevExpress.devices.current(),
+        iosVersion = DevExpress.devices.iosVersion();
+    if(device.platform === "ios" && iosVersion && iosVersion[0] === 7) {
+        $(".dx-viewport")
+            .removeClass("dx-theme-ios")
+            .addClass("dx-theme-ios7");
+    }
 
     TipCalculator.app.router.register(":view", { view: "home" });
     TipCalculator.app.navigate();   

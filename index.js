dd9856f34336e738cc19b0661abc7a13ca108af1
BingoRUS <bingofirst@gmail.com>
2014-01-20 16:08:33 +0400

    Update to PhoneJS 13.2.6

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 ﻿"use strict";
 
-window.TipCalculator = {};
+window.TipCalculator = window.TipCalculator || {};
 
 $(function() {
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    // DevExpress.devices.current({ platform: "generic" });
+
     document.addEventListener("deviceready", function() { navigator.splashscreen.hide(); });
 
     TipCalculator.app = new DevExpress.framework.html.HtmlApplication({
         namespace: TipCalculator,
-        
-        defaultLayout: "empty"
+        navigationType: TipCalculator.config.navigationType
     });
-
-    // enable iOS7 theme
-    var device = DevExpress.devices.current(),
-        iosVersion = DevExpress.devices.iosVersion();
-    if(device.platform === "ios" && iosVersion && iosVersion[0] === 7) {
-        $(".dx-viewport")
-            .removeClass("dx-theme-ios")
-            .addClass("dx-theme-ios7");
-    }
 
     TipCalculator.app.router.register(":view", { view: "home" });
     TipCalculator.app.navigate();   
 });
+
+Globalize.culture(navigator.language || navigator.browserLanguage);
+//Globalize.culture("fr");
